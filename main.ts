05743cbac4da844299cfb4121b437b1e09ded1fe
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-09-29 13:07:04 -0400

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sophie
+ * Created on: Sep 2023
+ * This program controls LED through a pin
 */
 
-basic.showString('Hello, World!')
+// on start
+basic.clearScreen()
+basic.showIcon(IconNames.Heart)
+basic.pause(1000)
+pins.digitalWritePin(DigitalPin.P16, 0)
+
+// Pin on
+input.onButtonPressed(Button.A, function() {
+  pins.digitalWritePin(DigitalPin.P16, 1)
+  basic.showIcon(IconNames.Yes)
+})
+
+// Pin off
+input.onButtonPressed(Button.B, function () {
+  pins.digitalWritePin(DigitalPin.P16, 0)
+  basic.showIcon(IconNames.No)
+})

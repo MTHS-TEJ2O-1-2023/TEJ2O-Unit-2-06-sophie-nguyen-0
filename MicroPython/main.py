007c1d5454dbbb50b05ed17bf22ca0c609ae3b80
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-09-29 13:26:32 -0400

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,10 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Sophie N
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear()

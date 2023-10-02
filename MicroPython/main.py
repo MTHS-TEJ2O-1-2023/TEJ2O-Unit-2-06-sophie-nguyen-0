58f491946ae9513bba95597ee443449d30db90c4
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-02 12:44:22 -0400

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,5 +6,16 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
-
+// setup
 display.clear()
+display.show(Image.HEART)
+
+while true:
+    // on
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.show(image.YES)
+    // off
+    if button_b.is_pressed():
+        pin16.write_digital(0)
+        display.show(image.NO)
